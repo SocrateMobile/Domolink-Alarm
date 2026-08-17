90ba8f2fb8fcc652fc2e172fbab7de2d93f62653
socrate <Torjku11APL&>
2026-08-17 23:26:53 +0200

    Fix HACS structure

---
+++ custom_components/domolink_alarm/const.py
@@ -0,0 +1,25 @@
+"""Constants for the Domolink Alarm integration."""
+
+DOMAIN = "domolink_alarm"
+
+CONF_OPENING_SENSORS = "opening_sensors"
+CONF_MOTION_SENSORS = "motion_sensors"
+CONF_CAMERAS = "cameras"
+CONF_TAMPER_SENSORS = "tamper_sensors"
+CONF_KEYPADS = "keypads"
+
+CONF_SIRENS = "sirens"
+CONF_MEDIA_PLAYERS = "media_players"
+CONF_NOTIFY_SERVICES = "notify_services"
+
+CONF_MAIN_CODE = "main_code"
+CONF_SECONDARY_CODES = "secondary_codes"
+CONF_BYPASS_ALLOWED = "bypass_allowed"
+CONF_EXIT_DELAY = "exit_delay"
+CONF_ENTRY_DELAY = "entry_delay"
+CONF_SIREN_DURATION = "siren_duration"
+
+DEFAULT_EXIT_DELAY = 30
+DEFAULT_ENTRY_DELAY = 30
+DEFAULT_SIREN_DURATION = 180
+DEFAULT_BYPASS_ALLOWED = False
