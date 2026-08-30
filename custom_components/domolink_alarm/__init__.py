0645b15e0fef8c2b666cdb5cee342731f524dd96
socrate <Torjku11APL&>
2026-08-30 23:21:04 +0200

    Add translations for English, Spanish, German, Ukrainian, and updated Italian

--- custom_components/domolink_alarm/__init__.py
+++ custom_components/domolink_alarm/__init__.py
@@ -39,7 +39,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
                 config={
                     "_panel_custom": {
                         "name": "domolink-panel",
-                        "module_url": "/domolink_alarm_panel/domolink-panel.js?v=0.9.19",
+                        "module_url": "/domolink_alarm_panel/domolink-panel.js?v=0.9.20",
                     }
                 },
                 require_admin=False,
